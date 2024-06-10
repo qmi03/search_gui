f40cc2446adc00a12670c1831050778a467408b6
Qmi <minh.pham1126@gmail.com>
2024-06-10 15:10:42 +0700

    feat: define look up class

--- excel/lookup_excel.py
+++ excel/lookup_excel.py
@@ -1,11 +1,37 @@
 import glob
 import os
 import sys
+from re import A
 
 import openpyxl
 
 
 class DirectoryExcel:
+    class SearchResult:
+        def __init__(self,file,sheet,row,col,val) -> None:
+            self.file = file
+            self.sheet = sheet
+            self.row = row
+            self.col = col
+            self.val = val
+        def __str__(self):
+            return f"File: {self.file}, Sheet: {self.sheet}, Row: {self.row}, Column: {self.col}, Value: {self.val}"
+
+        def get_file(self):
+            return self.file
+
+        def get_sheet(self):
+            return self.sheet
+
+        def get_row(self):
+            return self.row
+
+        def get_column(self):
+            return self.col
+
+        def get_value(self):
+            return self.val
+
     def __init__(self, root_directory: str, recursive: bool = False) -> None:
         self.root_dir = os.path.abspath(root_directory)
         self.recursive = recursive
@@ -31,9 +57,16 @@ class DirectoryExcel:
             else:
                 search_pattern = os.path.join(self.root_dir, extension)
                 self.excel_files.extend(glob.glob(search_pattern, recursive=False))
+    def search_keyword (self,keyword:str):
 
 
-dir_excel = DirectoryExcel(".")
-print(dir_excel.root_dir)
-for excel_file in dir_excel.excel_files:
-    print(excel_file)
+
+
+if __name__ == "__main__":
+    recursive = False
+    if len(sys.argv) > 1:
+        recursive = sys.argv[1].lower() == "true"
+    dir_excel = DirectoryExcel(".", recursive)
+    print(dir_excel.root_dir)
+    for excel_file in dir_excel.excel_files:
+        print(excel_file)
